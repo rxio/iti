996f68e5cf56a46d7c497becf9a203eaaad5ef77
rxio <xiongrichard@gmail.com>
2018-07-10 20:58:44 -0400

    LinkedListQueue.java class is finished

--- LinkedListQueue.java
+++ LinkedListQueue.java
@@ -4,8 +4,10 @@ public class LinkedListQueue<E> implements Queue<E> {
 	private Node<E> tail;
 
 	public LinkedListQueue() {
+
 		head = null;
 		tail = null;
+
 	}
 
 	public boolean isEmpty() {
@@ -18,19 +20,32 @@ public class LinkedListQueue<E> implements Queue<E> {
 
 		head = null;
 		tail = null;
+
 	}
 
-	
+	public void enqueue( E obj ) {
+		
+		Node<E> newElement = new Node<E>(obj);
 
-	public void enqueue( E obj, Node<E> selectedNode ) {
-
-		if ( isEmpty() ) {
-			Node<E> newElement = new Node<E>(obj);
+		if ( isEmpty() ) {	
 			tail = newElement;
 			head = tail;
-		} else {
-			Node<E> newElement = new Node<E>( obj, selectedNode);
-			tail.next = newElement;
+		} else {	
+			tail.setNext( newElement );
+			tail = newElement;
+		}		
+
+	}
+
+	public void enqueue( E obj, Node<E> selectedNode ) {
+		
+		Node<E> newElement = new Node<E>( obj, selectedNode );
+
+		if ( isEmpty() ) {	
+			tail = newElement;
+			head = tail;
+		} else {	
+			tail.setNext( newElement );
 			tail = newElement;
 		}
 
@@ -42,6 +57,15 @@ public class LinkedListQueue<E> implements Queue<E> {
 			return null;
 		}
 
+		Node<E> toBeRemoved = head;
+		E savedData = toBeRemoved.getData();
+		head = head.getNext();
+
+		if ( head == null ) {
+			tail = null;
+		}
+
+		return savedData;
 		
 	}
 
